937b9cdd7c0d2c51f2cabfe6c799215aedaacd4b
Xuan Yiming <XUANYIMING@OUTLOOK.COM.PE>
2024-10-07 17:06:07 -0500

    tiktok test

--- social-management/app/api/tiktok/login/route.ts
+++ social-management/app/api/tiktok/login/route.ts
@@ -5,17 +5,22 @@ export async function GET(req: Request) {
 
     // TikTok OAuth URL with your client key and redirect URL
     const clientKey = process.env.TIKTOK_CLIENT_KEY ; // Replace with actual client key
-    const redirectUri = encodeURIComponent('http://localhost:3000/api/tiktok/callback');
+    const redirectUri = encodeURIComponent('https://helado-villaizan.vercel.app/api/tiktok/auth/callback');
     const state = csrfState; // Used to prevent CSRF attacks (can be random string)
-
+    const scope = [
+        'user.info.basic',
+        'video.upload',
+        'video.publish',
+        'video.list',
+    ]
     // TikTok's OAuth URL to initiate login
     const tiktokOauthUrl = process.env.TIKTOK_AUTH_API_URL 
     +`?`
     +`client_key=${clientKey}`
-    +`&scope=video.publish`
+    +`&scope=${scope.join('%2')}`
     +`&response_type=code`
-    +`&redirect_uri=${redirectUri}`
-    +`&state=${state}`;
+    +`&redirect_uri=${redirectUri}`;
+     +`&state=${state}`;
 
     console.log('TikTok OAuth URL:', tiktokOauthUrl);
     // Redirect to TikTok OAuth URL
