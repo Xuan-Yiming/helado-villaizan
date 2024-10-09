205d1dfe0c666978690929f2277acc64ca0c9fd6
fatimacf <fatima.cruzalegui@pucp.edu.pe>
2024-10-09 16:26:21 -0500

    front login

--- social-management/app/ui/cuentas/vincular-cuenta.tsx
+++ social-management/app/ui/cuentas/vincular-cuenta.tsx
@@ -5,6 +5,7 @@ import GoogleLogo from "@/app/ui/icons/google"
 import FacebookLogo from "@/app/ui/icons/facebook"
 import InstagramLogo from "@/app/ui/icons/instagram"
 import TiktokLogo from "@/app/ui/icons/tiktok"
+import axios from 'axios';
 
 interface Account {
     name: string;
@@ -18,21 +19,25 @@ interface VincularCuentaProps {
 const VincularCuenta: React.FC<VincularCuentaProps> = ({ accounts }) => {
     // Función para redirigir a la autenticación de cada red social
     const handleVincular = (accountName: string) => {
+        try {
         switch (accountName) {
             case 'Facebook':
-                window.location.href = `https://www.facebook.com/v12.0/dialog/oauth?client_id=TU_CLIENT_ID&redirect_uri=TU_REDIRECT_URI&scope=email,public_profile`;
+                window.location.href = '/accounts/facebook/login/';
                 break;
             case 'Google':
                 window.location.href = `https://accounts.google.com/o/oauth2/auth?client_id=TU_CLIENT_ID&redirect_uri=TU_REDIRECT_URI&scope=email profile&response_type=code`;
                 break;
             case 'Instagram':
-                window.location.href = `https://api.instagram.com/oauth/authorize?client_id=TU_CLIENT_ID&redirect_uri=TU_REDIRECT_URI&scope=user_profile,user_media&response_type=code`;
+                window.location.href = '/accounts/instagram/login/';
                 break;
             case 'Tiktok':
                 window.location.href = `https://www.tiktok.com/auth/authorize?client_key=TU_CLIENT_KEY&redirect_uri=TU_REDIRECT_URI&scope=user.info.basic&response_type=code`;
                 break;
             default:
                 break;
+        }
+        } catch (error) {
+            console.error("Error al vincular cuenta", error);
         }
     };
 
