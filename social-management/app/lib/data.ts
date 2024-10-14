d741311a40711649335f4f9d803cf67b7c136e26
Xuan Yiming <89474387+Xuan-Yiming@users.noreply.github.com>
2024-10-14 17:27:42 -0500

    update posts UI

---
+++ social-management/app/lib/data.ts
@@ -0,0 +1,3 @@
+export async function load_posts(page: number, redSocial: string, tipoPublicacion: string, estado: string, tags: string) {
+    
+}
--- social-management/app/ui/publicar/post-card.tsx
+++ social-management/app/ui/publicar/post-card.tsx
@@ -1,68 +1,62 @@
 import React from 'react';
 import Image from 'next/image';
 
-import { 
-    HeartIcon as SolidHeartIcon, 
-    StarIcon as SolidStarIcon, 
+import {  
     ArrowTopRightOnSquareIcon 
 } from '@heroicons/react/24/solid'; 
 
 import { 
-    HeartIcon as OutlinedHeartIcon,
-    StarIcon as OutlinedStarIcon
+    PaperAirplaneIcon
 } 
 from '@heroicons/react/24/outline'; 
+import Link from 'next/link';
 
 interface PostCardProps {
-    userName: string;
     postTime: string;
     image: string;
-    comment: string;
     content: string;
     socialIcon?: React.ReactNode;
-    liked?: boolean;
-    saved?: boolean;
-
+    ifPosted: boolean;
+    link: string;
 }
 
-const PostCard: React.FC<PostCardProps> = ({ userName, postTime,image , comment, content, socialIcon, liked, saved }) => (
-    <div
-        className="border rounded border-gray-300 p-4 mb-4 rounded-2xl bg-white max-w-sm mx-auto flex flex-col justify-between"
+const PostCard: React.FC<PostCardProps> = ({ postTime,image, content, socialIcon, link, ifPosted }) => (
+    <li
+        className=" min-w-full border rounded border-gray-300 p-4 m-1 rounded-xl bg-white mx-auto flex flex-col md:flex-row justify-between w-full md:w-3/4"
     >
-        <div className="flex justify-between">
-            <div className="flex items-center">
-                <div className="w-10 text-black md:w-10">
-                    {socialIcon}
-                </div>
-                <h3 className="font-bold">{userName}</h3>
-            </div>
-            <div className="bg-gray-200 rounded-full p-2 text-xs text-gray-700 post-time">{postTime}</div>
-        </div>
-        <p className="text-sm mt-2 text-gray-600">{comment}</p>
-        <Image 
-            src={image} 
-            alt="Post image" 
-            className="mt-4" 
-            width={500}
-            height={500}
-        />
-        <p className="mt-6 mb-6 space-x-4">{content}</p>
+        <div className='flex flex-row justify-between '>
+            <div className="flex justify-between w-full md:w-auto">
+                <div className="flex items-center">
+                    <div className="w-10 text-black md:w-10">
+                        {socialIcon}
+                    </div>
 
-        <div className="flex justify-end mt-auto">
-            <button className="flex items-center text-red-500 hover:text-red-700 mr-2">
-                {liked ? <SolidHeartIcon className="h-5 w-5 mr-1" /> : <OutlinedHeartIcon className="h-5 w-5 mr-1" />}
-                <span>Like</span>
-            </button>
-            <button className="flex items-center text-yellow-500 hover:text-yellow-700 mr-2">
-                {saved ? <SolidStarIcon className="h-5 w-5 mr-1" /> : <OutlinedStarIcon className="h-5 w-5 mr-1" />}
-                <span>Guardar</span>
-            </button>
-            <button className="flex items-center text-blue-500 hover:text-blue-700">
-                <ArrowTopRightOnSquareIcon className="h-5 w-5 mr-1" />
-                <span>Abrir</span>
-            </button>
+                    <Image 
+                        src={image} 
+                        alt="Post image" 
+                        className="mt-4 md:mt-0 md:ml-4 border rounded-md" 
+                        width={50}
+                        height={50}
+                    />
+                </div>
+            </div>
+
+            <div className='flex flex-col justify-between w-full md:w-auto'>
+                <p className="p-2 pb-0 space-x-4">
+                    {content.length > 50 ? `${content.substring(0, 50)}...` : content}
+                </p>
+                <div className="p-2 pt-0 text-xs text-gray-700">{postTime}</div>
+            </div>
         </div>
-    </div>
+
+        
+        <Link 
+            href={link}
+            className="flex items-center text-blue-500 hover:text-blue-700">
+             {ifPosted ? <ArrowTopRightOnSquareIcon className="h-5 w-5 mr-1" /> : <PaperAirplaneIcon className="h-5 w-5 mr-1" /> } 
+            <span>{ifPosted ? 'Abrir' : 'Publicar'}</span>
+        </Link>
+    </li>
 );
 
 export default PostCard;
