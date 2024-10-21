9b15b6ed1082422fa4a5015e7fcb8a5e428388d7
Xuan Yiming <89474387+Xuan-Yiming@users.noreply.github.com>
2024-10-21 15:03:31 -0500

    crear encuesta

--- social-management/app/ui/encuesta/encuesta-header.tsx
+++ social-management/app/ui/encuesta/encuesta-header.tsx
@@ -1,25 +1,44 @@
+import { useState, useEffect } from 'react';
 import { Encuesta } from "@/app/lib/types";
 
 interface EncuestaHeaderProps {
     encuesta: Encuesta | null;
+    onUpdate: (updatedEncuesta: Encuesta) => void; // Add onUpdate callback
 }
 
-export default function EncuestaHeader({ encuesta }: EncuestaHeaderProps) {
+export default function EncuestaHeader({ encuesta, onUpdate }: EncuestaHeaderProps) {
+    const [title, setTitle] = useState(encuesta?.title || '');
+    const [description, setDescription] = useState(encuesta?.description || '');
+    const [startDate, setStartDate] = useState(encuesta?.start_date || '');
+    const [endDate, setEndDate] = useState(encuesta?.end_date || '');
+
+    useEffect(() => {
+        onUpdate({
+            ...encuesta,
+            title,
+            description,
+            start_date: startDate,
+            end_date: endDate,
+        } as Encuesta);
+    }, [title, description, startDate, endDate]);
+
     return (
-        <div className="p-4  mx-auto">
+        <div className="p-4 mx-auto">
             <div className="card bg-white shadow-md rounded-lg p-6">
                 <h2 className="text-2xl font-bold mb-4">Información Basica</h2>
                 <input
                     type="text"
                     placeholder="Título"
-                    className="w-full p-2 mb-4 font-bold text-xl border-b-2 "
+                    className="w-full p-2 mb-4 font-bold text-xl border-b-2"
                     required
-                    value={encuesta?.title || ''}
+                    defaultValue={title}
+                    onChange={(e) => setTitle(e.target.value)}
                 />
                 <textarea
                     placeholder="Descripción"
                     className="w-full p-2 mb-4 border-b-2"
-                    value={encuesta?.description || ''}
+                    defaultValue={description}
+                    onChange={(e) => setDescription(e.target.value)}
                 />
                 <div className="flex justify-between mb-4">
                     <p className="font-bold"> Fecha de encuesta </p>
@@ -27,17 +46,19 @@ export default function EncuestaHeader({ encuesta }: EncuestaHeaderProps) {
                         <input
                             type="date"
                             placeholder="Start Date"
-                            className="w-1/2 p-2  mr-2"
+                            className="w-1/2 p-2 mr-2"
                             required
-                            value={encuesta?.start_date|| ''}
+                            defaultValue={startDate ? new Date(startDate).toISOString().split('T')[0] : ''}
+                            onChange={(e) => setStartDate(e.target.value)}
                         />
                         <p> a </p>
                         <input
                             type="date"
                             placeholder="End Date"
-                            className="w-1/2 p-2  ml-2"
+                            className="w-1/2 p-2 ml-2"
                             required
-                            value={encuesta?.end_date || ''}
+                            defaultValue={endDate ? new Date(endDate).toISOString().split('T')[0] : ''}
+                            onChange={(e) => setEndDate(e.target.value)}
                         />
                     </div>
                 </div>
--- social-management/app/ui/encuesta/encuesta-node.tsx
+++ social-management/app/ui/encuesta/encuesta-node.tsx
@@ -1,102 +1,167 @@
-
-import { useState } from "react"
-
-import { Question } from "@/app/lib/types"
+import { useState } from "react";
+import { Question } from "@/app/lib/types";
+import { TrashIcon, PlusCircleIcon } from "@heroicons/react/24/outline";
 
 interface EncuestaNodeProps {
-    question: Question
+  question: Question;
+  onDelete: (questionId: string) => void; // Add onDelete callback
+  onUpdate: (updatedQuestion: Question) => void; // Add onUpdate callback
+  isEditable: boolean;
 }
 
+export default function EncuestaNode({ question, onDelete, onUpdate, isEditable }: EncuestaNodeProps) {
+  const [questionType, setQuestionType] = useState(question.type);
+  const [options, setOptions] = useState(question.options || []);
+  const [title, setTitle] = useState(question.title);
 
-export default function EncuestaNode( {question}: EncuestaNodeProps){
+  function handleDeleteNode() {
+    if (question.id)
+        onDelete(question.id); // Call the onDelete callback with the question ID
+  }
 
-    const [questionType, setQuestionType] = useState(question.type);
+  function handleAddOption() {
+      setOptions([...options, '']);
+      onUpdate({ ...question, options: [...options, ''] });
+  }
 
-    function getQuestionType(questionType: string){
-        switch(questionType){
-            // open_text, single_choice, multiple_choice
-            case "open_text":
-                return (
-                    <input
-                    type="text"
-                    placeholder="Respuesta"
-                    className="text-xl font-bold text-gray-700 w-full border-b-2 mb-4"
-                    required={question.required}
-                    />
-                )
-            case "single_choice":
-                return (
-                    <div className="flex flex-col w-full">
-                        {question.options?.map((option, index) => (
-                            <div key={index} className="flex items-center">
-                                <input
-                                type="radio"
-                                name={question.title}
-                                value={option}
-                                required={question.required}
-                                />
-                                <label>{option}</label>
-                            </div>
-                        ))}
-                    </div>
-                )
-            case "multiple_choice":
-                return (
-                    <div className="flex flex-col w-full">
-                        {question.options?.map((option, index) => (
-                            <div key={index} className="flex items-center">
-                                <input
-                                type="checkbox"
-                                name={question.title}
-                                value={option}
-                                required={question.required}
-                                />
-                                <label>{option}</label>
-                            </div>
-                        ))}
-                    </div>
-                )
-            default:
-                return <div>Invalid question type</div>
-        }
-    }
+  function handleOptionChange(index: number, newValue: string) {
+    const updatedOptions = options.map((option, i) => (i === index ? newValue : option));
+    setOptions(updatedOptions);
+    onUpdate({ ...question, options: updatedOptions });
+  }
 
-    return(
-        <div className="p-4 mx-auto text-left"> {/* Added text-left class */}
-            <div className="card bg-white shadow-md rounded-lg p-6">
+  function handleTitleChange(e: React.ChangeEvent<HTMLInputElement>) {
+    setTitle(e.target.value);
+    onUpdate({ ...question, title: e.target.value });
+  }
 
-                <div className="flex justify-between mb-4">
-                    <p className="font-bold ml-auto"> Tipo de Pregunta: </p>
-                    <select
-                        title="Tipo de pregunta"
-                        className="text-md font-bold text-gray-700  border-b-2 mb-4 ml-5"
-                        value={questionType}
-                        onChange={(e) => {
-                            setQuestionType(e.target.value);
-                            
-                            // Update the question type in the parent component
-                            // You can pass a callback function from the parent component to handle the type change
-                        }}
-                    >
-                        <option value="open_text">Texto</option>
-                        <option value="single_choice">Opción Única</option>
-                        <option value="multiple_choice">Multiple Opciones</option>
-                    </select>
-                </div>
-
-
+  function getQuestionType(questionType: string) {
+    switch (questionType) {
+      // open_text, single_choice, multiple_choice
+      case "open_text":
+        return (
+          <input
+            type="text"
+            placeholder="Respuesta"
+            className="text-md text-gray-700 w-full border-b-2 mb-4"
+            required={question.required}
+            disabled={isEditable}
+          />
+        );
+      case "single_choice":
+        return (
+          <div className="flex flex-col w-full">
+            {options.map((option, index) => (
+              <div key={index} className="flex items-center">
                 <input
-                type="text"
-                placeholder="Pregunta"
-                className="text-xl font-bold text-gray-700 w-full border-b-2 mb-4"
-                value={question.title}
-                required
+                  type="radio"
+                  name={question.title}
+                  value={option}
+                  required={question.required}
                 />
-                <div className="flex flex-col w-full">
-                    {getQuestionType(questionType)}
-                </div>
-            </div>
-        </div>
-    )
+                <input
+                  type="text"
+                  value={option}
+                  onChange={(e) => handleOptionChange(index, e.target.value)}
+                  className="ml-2 border-b-2"
+                />
+              </div>
+            ))}
+            <button
+              type="button"
+              className="flex items-center mt-2 text-blue-500"
+              onClick={handleAddOption}
+            >
+              <PlusCircleIcon className="h-5 w-5 mr-2" />
+              Add Option
+            </button>
+          </div>
+        );
+      case "multiple_choice":
+        return (
+          <div className="flex flex-col w-full">
+            {options.map((option, index) => (
+              <div key={index} className="flex items-center">
+                <input
+                  type="checkbox"
+                  name={question.title}
+                  value={option}
+                  required={question.required}
+                />
+                <input
+                  type="text"
+                  value={option}
+                  onChange={(e) => handleOptionChange(index, e.target.value)}
+                  className="ml-2 border-b-2"
+                />
+              </div>
+            ))}
+            <button
+              type="button"
+              className="flex items-center mt-2 text-blue-500"
+              onClick={handleAddOption}
+            >
+              <PlusCircleIcon className="h-5 w-5 mr-2" />
+              Add Option
+            </button>
+          </div>
+        );
+      default:
+        return <div>Invalid question type</div>;
+    }
+  }
 
+  return (
+    <div className="p-4 mx-auto text-left">
+      <div className="card bg-white shadow-md rounded-lg p-6">
+        <div className="flex justify-between mb-4">
+            <div className="flex items-center">
+            <input type="checkbox" name="requiredCheck" id="" checked={question.required} />
+            <label className="ml-2">Obligatorio</label>
+            </div>
+
+            <div className="flex justify-between">
+            <p className="font-bold ml-auto"> Tipo de Pregunta: </p>
+          <select
+            title="Tipo de pregunta"
+            className="text-md font-bold text-gray-700 border-b-2 mb-4 ml-5"
+            value={questionType}
+            onChange={(e) => {
+              setQuestionType(e.target.value);
+              onUpdate({ ...question, type: e.target.value });
+            }}
+          >
+            <option value="open_text">Texto</option>
+            <option value="single_choice">Opción Única</option>
+            <option value="multiple_choice">Multiple Opciones</option>
+          </select>
+            </div>
+
+        </div>
+
+        <input
+          type="text"
+          placeholder="Pregunta"
+          className="text-xl font-bold text-gray-700 w-full border-b-2 mb-4"
+          value={title}
+          onChange={handleTitleChange}
+          required
+        />
+        <div className="flex flex-col w-full">{getQuestionType(questionType)}</div>
+
+        <div className="flex items-center">
+
+
+            <button
+              className="flex items-center ml-5 rounded px-4 py-2 text-[#BD181E] ml-auto"
+              onClick={handleDeleteNode}
+            >
+              <TrashIcon className="h-5 w-5 mr-2" />
+              Eliminar
+            </button>
+        </div>
+
+      </div>
+    </div>
+  );
 }
