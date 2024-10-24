5a6d1a10436113eb490a803e8e4baf0a546722c3
fatimacf <fatima.cruzalegui@pucp.edu.pe>
2024-10-24 18:05:45 -0500

    Revert "fechas update"
    This reverts commit 13c1fe682422b04f2080058327b0ec3aec729914.

--- social-management/tailwind.config.js
+++ social-management/tailwind.config.js
@@ -1,10 +1,7 @@
 /** @type {import('tailwindcss').Config} */
+import 'tailwindcss/tailwind.css';
 module.exports = {
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: [],
   theme: {
     extend: {},
   },
