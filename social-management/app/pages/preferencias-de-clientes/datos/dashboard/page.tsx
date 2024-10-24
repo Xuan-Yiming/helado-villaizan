1daaa03cd6a2002e8d29b71b5965895beccbf9a6
fatimacf <fatima.cruzalegui@pucp.edu.pe>
2024-10-24 11:55:03 -0500

    ventas

---
+++ social-management/app/pages/preferencias-de-clientes/datos/dashboard/page.tsx
@@ -0,0 +1,10 @@
+"use client"; 
+import React from 'react';
+export default function DashboardPage() {
+    return (
+        <div>
+            <h1>Bienvenido al Dashboard</h1>
+            <p>Esta es la página de dashboard.</p>
+        </div>
+    );
+}
